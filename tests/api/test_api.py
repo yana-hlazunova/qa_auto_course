2dad6692ed9fb7fded82ce714a8d58821f6db619
Yana Hlazunova <yanah.qaengineer@gmail.com>
2024-11-26 18:33:07 +0200

    Added fixtures & markers

--- tests/api/test_api.py
+++ tests/api/test_api.py
@@ -1,3 +1,16 @@
-def test_check_math():
-    assert 7*7 == 49
+#testing results by calling this tests via PyTest
 
+import pytest
+
+@pytest.mark.change
+def test_remove_name(user):
+    user.name = ''
+    assert user.name == ''
+
+@pytest.mark.check
+def test_name(user):
+    assert user.name == 'Yana'
+
+@pytest.mark.check
+def test_second_name(user):
+    assert user.second_name == 'Hlazunova'
